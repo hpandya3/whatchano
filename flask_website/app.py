d99f9a5fe012eeaca3bb7c8ba6eadd4297b9a11e
shiva115 <aditya_mereddy@hotmail.com>
2017-11-18 11:20:08 +1000

    Basic dir setup for flask

--- flask_website/app.py
+++ flask_website/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def init():
-    return 'Hello, is it me you\'re looking for?'
+    return render_template('index.html')
+   #return 'Hello, is it me you\'re looking for?'
