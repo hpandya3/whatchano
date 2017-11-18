3ae593b8472b4a8afc9f7ed3ca075eac58b19bcf
Harsh Pandya <hpand3@gmail.com>
2017-11-18 14:35:30 +1000

    Added requirements, preprocessing commands, sentiment analysis commands

---
+++ flask_website/test.py
@@ -0,0 +1,4 @@
+from twitter.twitter import getUserTweets
+
+getUserTweets('DanBilzerian', 5)
+
--- flask_website/twitter/twitter.py
+++ flask_website/twitter/twitter.py
@@ -1,12 +1,24 @@
 import tweepy
 import local_settings as ls
+from preprocessing.preprocessing import preprocess
+from sentiment_analysis.sentiment import getSentiment
+
 
 # OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(ls.TWITTER_CONSUMER_KEY, ls.TWITTER_CONSUMER_SECRET)
-auth.set_access_token(ls.TWITTER_ACCESS_TOKEN, ls.TWITTER_ACCESS_TOKEN_SECRET)
+AUTH = tweepy.OAuthHandler(ls.TWITTER_CONSUMER_KEY, ls.TWITTER_CONSUMER_SECRET)
+AUTH.set_access_token(ls.TWITTER_ACCESS_TOKEN, ls.TWITTER_ACCESS_TOKEN_SECRET)
 
 # Creation of the actual interface, using authentication
-api = tweepy.API(auth)
+API = tweepy.API(AUTH)
 
-# Sample method, used to update a status
-api.update_status('#internetfreedomhackathon')
+def getUserTweets(screenName, count):
+  tweets = API.user_timeline(screen_name=screenName, count=count)
+  print(tweets[0].user.name) # Persons Name
+  print(tweets[0].user.profile_image_url) # Persons Image
+  for tweet in tweets:
+    decodedText = tweet.text.encode('ascii', 'ignore').decode('utf-8')
+    preprocessedText = preprocess(decodedText)
+    print(preprocessedText)
+    print(getSentiment(preprocessedText))
+
+  return tweets
