79006066a5ebf3f4d8d9d98fc0bf8533e46b827b
Clyde Smets <clyde.smets@gmail.com>
2017-11-19 18:23:18 +1000

    Removed dummy test files

--- flask_website/test.py
+++
@@ -1,4 +0,0 @@
-from twitter.tweetcontainer import TweetContainer
-tweetContainer = TweetContainer('realDonaldTrump')
-tweetContainer.load(20)
-print(tweetContainer.toDict(3))
--- flask_website/test2.py
+++
@@ -1,4 +0,0 @@
-from instagram import get_worst_posts
-
-test = get_worst_posts('qldvotes', 'tag', 6)
-print(test)
