856e5da46979c808dba70e9cf17331bb1774b036
Harsh Pandya <hpand3@gmail.com>
2017-11-19 12:14:26 +1000

    Implemented twitter app with flask

--- flask_website/app.py
+++ flask_website/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from twitter.tweetcontainer import TweetContainer
+import json
 
 app = Flask(__name__)
 
@@ -12,9 +15,10 @@ def init():
 @app.route('/ipost/<username>')
 def get_igram_posts(username):
     # Get the instagram posts of the user
-    return 'Username %s' % username 
-
-@app.route('/tpost/<username>')
-def show_post(username):
-    # show the post with the given id, the id is an integer
     return 'Username %s' % username
+
+@app.route('/tpost/<username>/<count>')
+def show_tweets(username, count):
+    tweetContainer = TweetContainer(username)
+    tweetContainer.load(20)
+    return jsonify(tweetContainer.toDict(int(count)))
